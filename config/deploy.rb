a2d2eeff336bb4f608cb7900813f10c75ee6b055
Fabio Vilela <fabio@iproperty.com.au>
2013-02-28 13:26:53 +1100

    production changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,22 @@ set :application, "notification_centre"
 set :repository,  "https://github.com/fbvilela/notification_centre.git"
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 task :amazon do   
   set :rails_env, "production"
   ssh_options[:user] = "ec2-user"
   ssh_options[:keys] = [File.join("~/.ssh/iproperty-ec2.pem")]
-  role :web, "184.169.130.114" # Your HTTP server, Apache/etc
-  role :app, "184.169.130.114" # This may be the same as your `Web` server
-  role :db,  "184.169.130.114", :primary => true # This is where Rails migrations will run  
+  role :web, "184.169.130.114" 
+  role :app, "184.169.130.114" 
+  role :db,  "184.169.130.114", :primary => true 
+end
+
+task :production do   
+  default_run_options[:pty] = true
+  set :rails_env, "production"
+  ssh_options[:user] = "ec2-user"
+  ssh_options[:keys] = [File.join("~/.ssh/iproperty-sydney-ec2.pem")]
+  role :web, "54.252.86.61" 
+  role :app, "54.252.86.61" 
+  role :db,  "54.252.86.61", :primary => true   
 end
